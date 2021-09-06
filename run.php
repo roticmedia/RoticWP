ba37cd7c0ba16173afb3e29bdea471c6da00b071
Milad Xandi <stun.m_zandizand@riau.ac.ir>
2021-09-06 20:32:46 +0430

    B-Two plugin added

--- run.php
+++ run.php
@@ -4,7 +4,7 @@
 Plugin Name: Rotic Plugin
 Plugin URI: https://github.com/roticmedia/RoticWP/releases
 Description: Connect your website to the Rotic, because you contacted to the future..
-Version: 2.0.0
+Version: 2.0.2
 Author: Milad Xandi
 Author URI: http://rotic.ir
 License: MIT
